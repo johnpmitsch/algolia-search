63ee1c481b272be7608122b8ffb2f09b30b832d5
John Mitsch <jomitsch@redhat.com>
2021-02-25 08:06:13 -0500

    Fixed linting

--- src/search.js
+++ src/search.js
@@ -1,7 +1,7 @@
 import algoliasearch from 'algoliasearch/lite';
 
-const appId = process.env.REACT_APP_ALGOLIA_APPLICATION_ID
-const appKey = process.env.REACT_APP_ALGOLIA_SECRET_KEY
+const appId = process.env.REACT_APP_ALGOLIA_APPLICATION_ID;
+const appKey = process.env.REACT_APP_ALGOLIA_SECRET_KEY;
 const client = algoliasearch(appId, appKey);
 const index = client.initIndex('stg_choicemarket_products');
 
